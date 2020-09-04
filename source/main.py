f9d5f705dae8ce22efe790215d7c505acc43de48
Mårten Skogh <marten.skogh@gmail.com>
2020-09-04 19:28:38 +0200

    Fixed import for VQE wrapper

--- source/main.py
+++ source/main.py
@@ -5,7 +5,7 @@ import scipy as sp
 from pprint import pprint
 from timeit import default_timer as timer
 
-from QiskitVQEWrapper.vqe_wrapper import VQEWrapper
+from QiskitVQEWrapper.vqe_wrapper.VQE_wrapper import VQEWrapper
 from pyscf_helpers import make_reasonable_mol_name
 
 
@@ -17,9 +17,10 @@ vqe_wrapper.freeze_core = True
 
 parameter_transfer = False
 coords = np.linspace(0.1, 2.0, num=100)
-mol_str = "Li 0.0 0.0 0.0; H {} 0.0 0.0"
+mol_str = "H 0.0 0.0 0.0; H {} 0.0 0.0"
+filename = "PLEASE_DELETE_ME.txt"
 #filename = f'results/{make_reasonable_mol_name(mol_str)}_{parameter_transfer}.csv'
-#output_file = open(filename, 'w')
+output_file = open(filename, 'w')
 
 print(f'Results will be written to {filename}')
 
@@ -36,6 +37,6 @@ for coord in coords:
     energy = result['nuclear_repulsion_energy'] + result['computed_electronic_energy']
 
     print(f'{coord:.4}, {vqe_wrapper.vqe_time}, {cost_function_evals}, {energy}')
-#    output_file.write(f'{coord}, {vqe_wrapper.vqe_time}, {cost_function_evals}, {energy}\n')
+    output_file.write(f'{coord}, {vqe_wrapper.vqe_time}, {cost_function_evals}, {energy}\n')
 
 output_file.close()
