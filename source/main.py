Fixed energy calculation bug

source/main.py
@@ -17,9 +17,8 @@ vqe_wrapper.freeze_core = True
 
 parameter_transfer = False
 coords = np.linspace(0.1, 2.0, num=100)
-mol_str = "H 0.0 0.0 0.0; H {} 0.0 0.0"
-filename = "PLEASE_DELETE_ME.txt"
-#filename = f'results/{make_reasonable_mol_name(mol_str)}_{parameter_transfer}.csv'
+mol_str = "Li 0.0 0.0 0.0; H {} 0.0 0.0"
+filename = f'results/{make_reasonable_mol_name(mol_str)}_{parameter_transfer}.csv'
 output_file = open(filename, 'w')
 
 print(f'Results will be written to {filename}')
@@ -34,7 +33,7 @@ for coord in coords:
     if parameter_transfer: 
         vqe_wrapper.initial_point = vqe_wrapper.vqe_algo.optimal_params
 
-    energy = result['nuclear_repulsion_energy'] + result['computed_electronic_energy']
+    energy = result['nuclear_repulsion_energy'] + result['computed_electronic_energy'] + result['frozen_extracted_energy']
 
     print(f'{coord:.4}, {vqe_wrapper.vqe_time}, {cost_function_evals}, {energy}')
     output_file.write(f'{coord}, {vqe_wrapper.vqe_time}, {cost_function_evals}, {energy}\n')